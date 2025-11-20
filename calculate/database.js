d5bcc423b983c82702509cb943e05f168add247a
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-11-20 21:44:45 +0530

    Did CRUD operation on collection

--- calculate/database.js
+++ calculate/database.js
@@ -4,17 +4,55 @@ const {MongoClient}=require("mongodb");
 const url =
     "mongodb+srv://Rudra01:Rpsj123@nodeself.whijtvy.mongodb.net/";
 
+
 const client = new MongoClient(url);
 
 //Database Name
-const dbName = "nodeSelfDB";
+const dbName = "FirstDataBase";
  
 async function main(){
     //Use connect method to connect to the server
     await client.connect();
     console.log("Connected successfully to server");
     const db = client.db(dbName);
-    const collection = db.collection("users");
+    const collection = db.collection("User");
+
+    //Insert
+    const data ={
+        _id:"01",
+        firstname: "Kanak",
+        lastname: "Sharma",
+        city: "Haridwar"
+    }
+
+    // const insertResult = await collection.insertOne(data);
+    // console.log("Inserted doc =>", insertResult);
+    
+    // //COUNT 
+    // const count = await collection.countDocuments({});
+    
+    
+    // //Read
+    // //Finding All Documents
+    // const findResult = await collection.find({}).toArray();
+    // console.log("Found documents =>", findResult);
+    // console.log("Total number of documents:", count);
+
+    //Find all the documents with city Haridwar
+    // const hardidwarUsers = await collection.find({city: "Haridwar"}).toArray();
+    // console.log("Users from Haridwar =>", hardidwarUsers);
+    
+    //Update Documents
+    // const updateResult = await collection.updateMany(
+    //     {city: "Haridwar"},
+    //     {$set: {city: "Gwalior"}}
+    // );
+    // console.log("Updated documents =>", updateResult);
+
+    //DELETE Documents
+    const deleteResult = await collection.deleteOne({_id: "01"});
+    console.log("Deleted document =>", deleteResult);
+    
     return 'done.';
 }
 
